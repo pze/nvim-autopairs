76e8d950d93bbe2c13ebc58d95356f30665a594f
Towry Wang <8279858+towry@users.noreply.github.com>
2023-01-10 23:01:30 +0800

    Fix error when ts get_parser return nil

--- lua/nvim-autopairs/ts-conds.lua
+++ lua/nvim-autopairs/ts-conds.lua
@@ -1,7 +1,7 @@
 local _, ts_utils = pcall(require, 'nvim-treesitter.ts_utils')
 
 local log = require('nvim-autopairs._log')
-local parsers = require'nvim-treesitter.parsers'
+local parsers = require 'nvim-treesitter.parsers'
 local utils = require('nvim-autopairs.utils')
 local ts_query = vim.treesitter.query
 
@@ -9,9 +9,9 @@ local conds = {}
 
 conds.is_endwise_node = function(nodes)
     if nodes == nil then return function() return true end end
-    if type(nodes) == 'string' then nodes = {nodes} end
+    if type(nodes) == 'string' then nodes = { nodes } end
 
-    return function (opts)
+    return function(opts)
         log.debug('is_endwise_node')
         if not opts.check_endwise_ts then return true end
         if nodes == nil then return true end
@@ -20,33 +20,32 @@ conds.is_endwise_node = function(nodes)
         parsers.get_parser():parse()
         local target = ts_utils.get_node_at_cursor()
         if target ~= nil and utils.is_in_table(nodes, target:type()) then
-            local text = ts_query.get_node_text(target) or {""}
+            local text = ts_query.get_node_text(target) or { "" }
             local last = text[#text]:match(opts.rule.end_pair)
             -- check last character is match with end_pair
             if last == nil then
                 return true
             end
-                -- log.debug('last:' .. last)
-                -- if match then we need tocheck parent node
-                --  some time treesiter is group 2 node  then we need check that
-                local begin_target,_, end_target = target:range()
-                local begin_parent,_, end_parent = target:parent():range()
-                -- log.debug(target:range())
-                -- log.debug(ts_query.get_node_text(target))
-                -- log.debug(target:parent():range())
-                -- log.debug(ts_query.get_node_text(target:parent()))
-                if
-                    (
-                        begin_target ~= begin_parent
-                        and end_target == end_parent
-                    )
-                    or
-                    (end_parent - end_target == 1)
-                then
-                    return true
-                end
-                -- return true
-            else
+            -- log.debug('last:' .. last)
+            -- if match then we need tocheck parent node
+            --  some time treesiter is group 2 node  then we need check that
+            local begin_target, _, end_target = target:range()
+            local begin_parent, _, end_parent = target:parent():range()
+            -- log.debug(target:range())
+            -- log.debug(ts_query.get_node_text(target))
+            -- log.debug(target:parent():range())
+            -- log.debug(ts_query.get_node_text(target:parent()))
+            if (
+                begin_target ~= begin_parent
+                    and end_target == end_parent
+                )
+                or
+                (end_parent - end_target == 1)
+            then
+                return true
+            end
+            -- return true
+        else
         end
         return false
     end
@@ -104,13 +103,17 @@ conds.is_in_range = function(callback, position)
 end
 
 conds.is_ts_node = function(nodes)
-    if type(nodes) == 'string' then nodes = {nodes} end
+    if type(nodes) == 'string' then nodes = { nodes } end
     assert(nodes ~= nil, "ts nodes should be string or table")
-    return function (opts)
+    return function(opts)
         log.debug('is_ts_node')
-        if #nodes == 0 then return  end
+        if #nodes == 0 then return end
 
-        parsers.get_parser():parse()
+        local parser = parsers.get_parser()
+        if not parser then
+            return false
+        end
+        parser:parse()
         local target = ts_utils.get_node_at_cursor()
         if target ~= nil and utils.is_in_table(nodes, target:type()) then
             return true
@@ -120,11 +123,17 @@ conds.is_ts_node = function(nodes)
 end
 
 conds.is_not_ts_node = function(nodes)
-    if type(nodes) == 'string' then nodes = {nodes} end
+    if type(nodes) == 'string' then nodes = { nodes } end
     assert(nodes ~= nil, "ts nodes should be string or table")
-    return function (opts)
+    return function(opts)
         log.debug('is_not_ts_node')
-        if #nodes == 0 then return  end
+        if #nodes == 0 then return end
+
+        local parser = parsers.get_parser()
+        if not parser then
+            return false
+        end
+        parser:parse()
 
         parsers.get_parser():parse()
         local target = ts_utils.get_node_at_cursor()
